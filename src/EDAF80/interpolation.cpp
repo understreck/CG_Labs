b84711730aac4bc2e31b3a6d5ba11cb27d4b9d9f
understreck <pqfrml@gmail.com>
2022-09-05 13:52:35 +0200

    Add clang-tidy and clang-format

--- src/EDAF80/interpolation.cpp
+++ src/EDAF80/interpolation.cpp
@@ -3,15 +3,19 @@
 glm::vec3
 interpolation::evalLERP(glm::vec3 const& p0, glm::vec3 const& p1, float const x)
 {
-	//! \todo Implement this function
-	return glm::vec3();
+    //! \todo Implement this function
+    return glm::vec3();
 }
 
 glm::vec3
-interpolation::evalCatmullRom(glm::vec3 const& p0, glm::vec3 const& p1,
-                              glm::vec3 const& p2, glm::vec3 const& p3,
-                              float const t, float const x)
+interpolation::evalCatmullRom(
+        glm::vec3 const& p0,
+        glm::vec3 const& p1,
+        glm::vec3 const& p2,
+        glm::vec3 const& p3,
+        float const t,
+        float const x)
 {
-	//! \todo Implement this function
-	return glm::vec3();
+    //! \todo Implement this function
+    return glm::vec3();
 }
